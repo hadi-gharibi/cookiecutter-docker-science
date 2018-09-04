817fac3548ebd6ad37493de0c15e3540c61c1761
hadi <hady.gharibi@gmail.com>
2018-09-04 16:58:27 +0430

    fix bug install python3.6

--- {{ cookiecutter.project_slug }}/docker/setup.sh
+++ {{ cookiecutter.project_slug }}/docker/setup.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install software-properties-common python-software-properties -y
 apt-add-repository ppa:deadsnakes/ppa
 apt-get update
-apt-get install python3.6
+apt-get install python3.6 -y
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6
 update-alternatives --config python3
